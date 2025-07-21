3fdaa34d89492e76c73c9348c5670545758a9cf3
Bayu Setiadji <bayusetiadji69@gmail.com>
2025-07-21 13:28:10 +0700

    update base url

--- src/utils/api/axios.js
+++ src/utils/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://62b9c510061d.ngrok-free.app/api",
 });
 
 instance.interceptors.request.use((config) => {
