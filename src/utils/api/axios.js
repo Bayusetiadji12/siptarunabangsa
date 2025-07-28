f93bdd89198cf33b02e477ce634adc1dc6d8d5e8
Bayu Setiadji <bayusetiadji69@gmail.com>
2025-07-28 16:26:46 +0700

    setting railway url

--- src/utils/api/axios.js
+++ src/utils/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://siptarunabangsa-be-production.up.railway.app/api",
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://siptarunabangsa-be-production.up.railway.app/api",
+  // baseURL: "http://localhost:4000/api",
 });
 
 instance.interceptors.request.use((config) => {
